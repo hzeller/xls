a7b51dec7c0fb2e004b954121ee0cc22b7a7e6fa
Henner Zeller <h.zeller@acm.org>
2023-02-11 21:22:53 -0800

    Clean up shell nix a bit.

--- shell.nix
+++ shell.nix
@@ -4,37 +4,41 @@
 
 # Use ccache stddev, so that a bazel clean makes it much
 # cheaper to rebuild the world.
+#
 # However, this requires that you add the following line
 # to your ~/.bazelrc
-# common --sanbdox_writeable_path=/your/home/directory/.cache/ccache
+# build --sanbdox_writeable_path=/your/home/directory/.cache/ccache
 
 { pkgs ? import <nixpkgs> {} }:
 pkgs.ccacheStdenv.mkDerivation {
   name = "ccache-enabled";
   buildInputs = with pkgs;
     [
-      git cacert
       bazel_5
+      jdk11
+      git cacert
+
       # For bazel to use the correct python,
       # use --repo_env PYTHON_BIN_PATH=`command -v python3` (see README)
       python310
-      jdk11
 
       # Python packages needed found with
-      # find . -name BUILD | xargs grep -h "requirement(" | sort | uniq
-      python310Packages.setuptools
-      python310Packages.scipy
-      python310Packages.numpy
+      #   find . -name BUILD | xargs grep -h "requirement(" | sort | uniq
+      # Mentioned here, but looks like PIP does not use the system
+      # packages yet. See below.
       python310Packages.click
-      python310Packages.portpicker
       python310Packages.flask
       python310Packages.itsdangerous
       python310Packages.jinja2
       python310Packages.markupsafe
+      python310Packages.numpy
+      python310Packages.portpicker
       python310Packages.psutil
       python310Packages.pyyaml
-      python310Packages.werkzeug
+      python310Packages.scipy
+      python310Packages.setuptools
       python310Packages.termcolor
+      python310Packages.werkzeug
 
       perl  # iverilog uses perl to create its config.h
 
@@ -43,13 +47,16 @@ pkgs.ccacheStdenv.mkDerivation {
       # Development support
       bazel-buildtools  # buildifier
     ];
+
    shellHook =
    ''
        # Due to the use of pip, some binary code is downloaded from the
-       # internet, introducing a non-hermetic build.
+       # Internet, making the build non-hermetic.
        # The downloaded code contains binary platform libraries that
        # assume they can link against local shared libraries. Allow this
-       # here for now, but this needs to be addressed.
+       # here for now by making them visible to the dynamic linker,
+       # but this needs to be addressed to only use hermetically
+       # supplied packages from the system.
        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.zlib}/lib
        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}/lib
    '';
